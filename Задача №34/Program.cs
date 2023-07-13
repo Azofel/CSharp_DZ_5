da6a6f80522cd70872bec392af2d21e6e06c3127
Azofel <136746291+Azofel@users.noreply.github.com>
2023-07-13 19:15:15 +0300

    CSharp_DZ_5
    CSharp_DZ_5

---
+++ Задача №34/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] randomArray = new int[a];
+
+void quantity(int a)
+{
+    for (int i = 0; i < a; i++)
+    {
+        randomArray[i] = new Random().Next(100,999);
+        Console.Write(randomArray[i] + " ");
+    }
+
+}
+
+int sum (int[] randomArray)
+{
+    int sum = 0;
+    for (int i = 0; i < randomArray.Length; i++)
+    {
+        if (randomArray[i] % 2 == 0)
+        sum++;
+    }
+    return sum;
+}
+
+quantity(a);
+Console.Write($"Количество чётных чисел в массиве: {sum(randomArray)}");
---
+++ Задача №36/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] randomArray = new int[a];
+
+void action(int a)
+{
+	for (int i = 0; i < a; i++)
+	{
+	randomArray[i] = new Random().Next();
+	Console.Write(randomArray[i] + " ");
+	}
+
+}
+
+int quantity(int[] randomArray)
+{
+	int sum = 0;
+	int i = 0;
+	while (i < randomArray.Length)
+	{
+		sum = sum + randomArray[i];
+		i = i + 2;
+	}
+	return sum;
+}
+
+action(a);
+Console.Write($"Cумма элементов, стоящих на нечётных позициях: {quantity(randomArray)}");
